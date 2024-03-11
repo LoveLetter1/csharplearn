907adb01132ff32419eaf08e50f48265f6ebcbe6
LoveLetter1 <690944639@qq.com>
2024-03-11 11:31:37 +0800

    update

---
+++ Class/chapter8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Csharp_learn.Class
+{
+    // 类的继承 已存在的类叫做基类base class，新类成为派生类derived class
+    // 派生类的组成有：
+    // 本身声明的成员、基类的成员
+    public class SomeClass
+    {
+        public string Field1 = "base class field";
+
+        public void Method1(string value)
+        {
+            Console.WriteLine($"Base class -- method1:{value}");
+        }
+    }
+
+    public class OtherClass : SomeClass
+    {
+        public string Field2 = "derived class field";
+        public void Method2(string value)
+        {
+            Console.WriteLine($"derived class -- method2:{value}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,109 +7,13 @@ namespace Csharp_learn
     {
         public static void Main(string[] args)
         {
-            PartClass partClass = new PartClass();
-            partClass.Add(3,4);
+            OtherClass otherClass = new OtherClass();
+            otherClass.Method1(otherClass.Field1);
+            otherClass.Method1(otherClass.Field2);
+            otherClass.Method2(otherClass.Field1);
+            otherClass.Method2(otherClass.Field1);
         }
         
-        
     }
-
-    static public class Tool
-    {
-        static public void Change(Student student)
-        {
-            student.age = student.age + 5;
-        }
-        
-        static public void RefAsPara(ref Student student)
-        {
-            student.age = 50;
-            Console.WriteLine(student.age);
-            student = new Student();
-            Console.WriteLine(student.age);
-        }
-
-        static public void OutAsPara(out Student result)
-        {
-            result = new Student();
-            result.age = 50;
-        }
-
-        static public void ListPara(params int[] paras)
-        {
-            foreach (var i in paras)
-            {
-                    Console.WriteLine(i);
-            }
-        }
-
-        static public void ArrayPara(int[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] += 5;
-            }
-        }
-
-        static public int Add(int x, Student student = null)
-        {
-            return x;
-        }
-
-    }
-
-    public class Student
-    {
-        // const和static无法同时出现
-        // public const/static int 顺序比较优雅
-        public const int num = 100;
-        public int age = 10;
-        private int grade = 5;
-
-        public int Grade
-        {
-            get => grade;
-        }
-
-    }
-    public class Rectangle
-    {
-        private double a=3.6;
-        private double b=4.0;
-
-        public double Area
-        {
-            get
-            {
-                return a * b;
-            }
-        }
-    }
-    public class Time
-    {
-        // private字段使用_驼峰
-        // public字段和属性使用首字母大写
-        private DateTime _timeOfInstantiation;
-        private static Random _random;
-        public DateTime TimeOfInstantiation
-        {
-            get { return _timeOfInstantiation; }
-        }
-        //析构器
-        //public构造函数可以外部使用，private析构器只能类内部使用
-        public Time()
-        {
-            _timeOfInstantiation = DateTime.Now;
-        }
-        //静态析构函数初始化静态字段
-        static Time()
-        {
-            _random = new Random();
-        }
-
-        public int NextRandom()
-        {
-            return _random.Next();
-        }
-    }
+    
 }
